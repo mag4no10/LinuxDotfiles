a7ba3c0f0773b3273803fd983fd5b9da7ee6c088
Jonay Faas Garcia <90365667+mag4no10@users.noreply.github.com>
2022-10-07 14:12:37 +0100

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+# Terminate already running bar instances
+killall polybar -q
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar top's
+polybar top -c /home/$USER/.config/polybar/config.ini &
+
+# Launch polybar bottom's
+polybar bottom -c /home/$USER/.config/polybar/config.ini &
+#polybar twobottom -c /home/$USER/.config/polybar/config.ini &
+#polybar threebottom -c /home/$USER/.config/polybar/config.ini &
+
